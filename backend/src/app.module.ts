804da67d57608f2649b686637af6ff5219537ca9
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2023-07-20 23:25:12 +0900

    remove modules and unused api

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,24 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CacheModule } from '@nestjs/cache-manager';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { OidcJwtStrategy } from './guards/jwt.strategy';
-import { GroupsModule } from './groups/groups.module';
-import { NotesModule } from './notes/notes.module';
-import { UsersService } from './users/users.service';
+
 import { AzblobService } from './azblob/azblob.service';
+import { CommentsController } from './comments/comments.controller';
+import { CommentsService } from './comments/comments.service';
+import { DraftsController } from './drafts/drafts.controller';
 import { EsService } from './es/es.service';
-import { UserModule } from './user/user.module';
-import { DraftsModule } from './drafts/drafts.module';
-import { NotesService } from './notes/notes.service';
+import { FilesController } from './files/files.controller';
+import { GroupsController } from './groups/groups.controller';
 import { GroupsService } from './groups/groups.service';
 import { MembershipsService } from './memberships/memberships.service';
-import { FilesModule } from './files/files.module';
-import { CommentsService } from './comments/comments.service';
-import { CommentsModule } from './comments/comments.module';
+import { NotesService } from './notes/notes.service';
+import { NotesController } from './notes/notes.controller';
+import { UserController } from './user/user.controller';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
 
 @Module({
   imports: [
@@ -28,25 +27,25 @@ import { CommentsModule } from './comments/comments.module';
     }),
     CacheModule.register(),
     PassportModule,
-    UsersModule,
-    GroupsModule,
-    NotesModule,
-    UserModule,
-    DraftsModule,
-    FilesModule,
-    CommentsModule,
   ],
-  controllers: [AppController],
+  controllers: [
+    CommentsController,
+    DraftsController,
+    FilesController,
+    GroupsController,
+    NotesController,
+    UserController,
+    UsersController,
+  ],
   providers: [
-    AppService,
     OidcJwtStrategy,
-    UsersService,
     AzblobService,
+    CommentsService,
     EsService,
-    NotesService,
     GroupsService,
     MembershipsService,
-    CommentsService,
+    NotesService,
+    UsersService,
   ],
 })
 export class AppModule {}
--- backend/src/files/files.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FilesController } from './files.controller';
-import { AzblobService } from 'src/azblob/azblob.service';
-
-@Module({
-  controllers: [FilesController],
-  providers: [AzblobService],
-})
-export class FilesModule {}
