04ac95e802ab1c0d6481ac9190df6dd8a3db7bad
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2023-07-30 16:32:26 +0900

    update member page

---
+++ frontend/app/(main)/g/[handle]/members/loader.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { useSearchParams, usePathname } from 'next/navigation';
+import { useEnvironment } from '@/components/environment/providers';
+import { useAccount, useMsal } from '@azure/msal-react';
+import useSWR from 'swr';
+import { Group } from '@/types';
+import { swrMsalTokenFetcher } from '@/components/msal/fetchers';
+import { MemberList } from '@/components/groups/members/list';
+import { LinkPagination } from '@/components/paginations';
+
+export function Loader({ group, pathname, page, take }: { group: Group; pathname: string; page: number; take: number }) {
+  const environment = useEnvironment();
+  const { instance, accounts } = useMsal();
+  const account = useAccount(accounts[0] || {});
+
+  // calculate skip
+  const skip = (page - 1) * take;
+
+  // fetch data
+  const fetcher = swrMsalTokenFetcher(instance, account, environment);
+  const { data: members, isLoading } = useSWR<{ data: any[]; meta: { total: number } }>(
+    `${environment.BACKEND_ENDPOINT}/groups/${group.id}/members?take=${take}&skip=${skip}`,
+    fetcher,
+    { revalidateOnFocus: false },
+  );
+
+  // render loading
+  if (isLoading) {
+    return <div>loading...</div>;
+  }
+
+  // render error
+  if (!members || members.data.length === 0 || members.meta.total === 0) {
+    return <div>No members</div>;
+  }
+
+  return (
+    <>
+      <MemberList members={members.data} />
+      <LinkPagination pathname={pathname} page={page} total={members.meta.total} take={take} />
+    </>
+  );
+}
