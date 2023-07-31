dcf9b098ced64e5eef6caf58eb8c7e859e992f3e
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2023-07-31 20:06:33 +0900

    wip main header

--- frontend/app/(main)/page.tsx
+++ frontend/app/(main)/page.tsx
@@ -1,14 +1,25 @@
 import { classNames } from '@/utils';
+import Link from 'next/link';
 import { Inter } from 'next/font/google';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Page() {
   return (
-    <div className="bg-slate-100 print:bg-white">
-      <div className="max-w-screen-2xl mx-auto">
-        <div className="p-4 md:p-8">
-          <div className="bg-white rounded-md p-4">
-            <div className={classNames('text-xl font-bold text-center', inter.className)}>Dashbord Area</div>
+    <div>
+      {/* Header area */}
+      <div className="py-4 bg-white ring-1 ring-gray-200">
+        <div className="max-w-screen-2xl mx-auto">
+          <div className="px-4 lg:px-8">header area</div>
+        </div>
+      </div>
+
+      <div className="bg-slate-100 print:bg-white">
+        <div className="max-w-screen-2xl mx-auto">
+          <div className="p-4 md:p-8">
+            <div className="bg-white rounded-md p-4">
+              <div className={classNames('text-xl font-bold text-center', inter.className)}>Dashbord Area</div>
+            </div>
           </div>
         </div>
       </div>
