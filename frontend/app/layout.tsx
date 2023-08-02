6456b2eeea1bf95650c12c0ff37d58c9aaa45b9e
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2023-08-02 21:14:57 +0900

    change localhost to 127.0.0.1

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -8,7 +8,7 @@ const environment: EnvironmentContextType = {
   AAD_FRONTEND_CLIENT_ID: process.env.AAD_FRONTEND_CLIENT_ID || '',
   AAD_BACKEND_CLIENT_ID: process.env.AAD_BACKEND_CLIENT_ID || '',
   AAD_BACKEND_API_SCOPE_NAME: process.env.AAD_BACKEND_API_SCOPE_NAME || '',
-  BACKEND_ENDPOINT: process.env.BACKEND_ENDPOINT || 'http://localhost:3001',
+  BACKEND_ENDPOINT: process.env.BACKEND_ENDPOINT || 'http://127.0.0.1:3001',
   WEBSITE_NAME: process.env.WEBSITE_NAME || 'Circle',
 };
 
