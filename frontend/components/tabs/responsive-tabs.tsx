5779867c13fe5597295d425a97334763143c0f63
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2023-08-12 01:27:46 +0900

    update layout and fix router

--- frontend/components/tabs/responsive-tabs.tsx
+++ frontend/components/tabs/responsive-tabs.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import { classNames } from '@/utils';
-import { usePathname } from 'next/navigation';
-import { TabItem } from './types';
+import { Inter } from 'next/font/google';
 import Link from 'next/link';
-import React from 'react';
+import { usePathname, useRouter } from 'next/navigation';
+import { TabItem } from './types';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export function ResponsiveTabs({ tabs }: { tabs: TabItem[] }) {
+  const router = useRouter();
   const pathname = usePathname();
   tabs.forEach((tab) => {
     tab.current = tab.href === pathname;
@@ -22,8 +25,17 @@ export function ResponsiveTabs({ tabs }: { tabs: TabItem[] }) {
           <select
             id="current-tab"
             name="current-tab"
-            className="block w-full rounded-md border-0 py-1.5 pl-3 pr-10 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
+            className={classNames(
+              inter.className,
+              'block w-full rounded-md border-0 py-1.5 pl-3 pr-10 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600',
+            )}
             defaultValue={tabs.find((tab) => tab.current)?.name}
+            onChange={(e) => {
+              const href = tabs.find((tab) => tab.name === e.target.value)?.href;
+              if (href) {
+                router.push(href);
+              }
+            }}
           >
             {tabs.map((tab) => (
               <option key={tab.name}>{tab.name}</option>
@@ -40,7 +52,8 @@ export function ResponsiveTabs({ tabs }: { tabs: TabItem[] }) {
                   tab.current
                     ? 'border-indigo-500 text-indigo-600'
                     : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
-                  'whitespace-nowrap border-b-2 px-1 pb-4 text-sm font-medium',
+                  'whitespace-nowrap border-b-2 px-2 pb-2 text-sm font-semibold',
+                  inter.className,
                 )}
                 aria-current={tab.current ? 'page' : undefined}
               >
