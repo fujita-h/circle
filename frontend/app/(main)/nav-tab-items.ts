564f4ef0662d6f06b13f8d16ecffae2286732130
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2023-10-05 00:24:47 +0900

    update tab

--- frontend/app/(main)/nav-tab-items.ts
+++ frontend/app/(main)/nav-tab-items.ts
@@ -3,5 +3,5 @@ import { TabItem } from '@/components/tabs';
 export const navTabItems: TabItem[] = [
   { name: 'Portal', href: `/`, current: false },
   { name: 'Following', href: `/following`, current: false },
-  { name: 'Explore', href: `/notes/explore`, current: false },
+  { name: 'Explore', href: ['/notes/explore', '/groups/explore'], current: false },
 ];
